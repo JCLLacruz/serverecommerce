68df29584900a7651b30287600a52dcdeafb83b9
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-05-30 09:08:48 +0200

    order routes fixed

--- routes/orders.js
+++ routes/orders.js
@@ -4,9 +4,9 @@ const OrderController = require('../controllers/OrderController.js');
 const { authentication, isAuthor, isAdmin } = require('../middleware/authentication.js');
 const {uploadCommentImages} = require('../middleware/multer.js')
 
-router.post('/', authentication, );
-router.put('/id/:_id', authentication, isAdmin, );
-router.delete('/id/:_id', authentication, isAdmin, );
-router.get('/', authentication, );
+router.post('/', authentication, OrderController.create);
+router.put('/id/:_id', authentication, isAdmin, OrderController.update);
+router.delete('/id/:_id', authentication, isAdmin, OrderController.delete);
+router.get('/', authentication, OrderController.getAll);
 
 module.exports = router;
