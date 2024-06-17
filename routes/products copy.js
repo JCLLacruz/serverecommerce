49ca57443e68188fde30839dd28be7d6ff5ba8a3
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-06-17 12:02:08 +0200

    athentication no needed

--- routes/products copy.js
+++ routes/products copy.js
@@ -5,13 +5,13 @@ const { authentication,isAdmin } = require("../middleware/authentication.js");
 const { uploadUserProductImages } = require('../middleware/multer.js');
 
 
-router.post('/', ProductController.create);
-router.put('/id/:_id', ProductController.update);
-router.delete('/id/:_id', ProductController.delete);
+router.post('/',uploadUserProductImages.single('productImg'), authentication, isAdmin, ProductController.create);
+router.put('/id/:_id', authentication, isAdmin, ProductController.update);
+router.delete('/id/:_id', authentication, isAdmin, ProductController.delete);
 router.get('/', ProductController.getAll);
 router.get('/title/:title', ProductController.getProductsByTitle);
 router.get('/id/:_id', ProductController.getById);
 router.put('/like/:_id', ProductController.like);
-router.delete('/like/:_id', ProductController.dislike);
+router.delete('/like/:_id', authentication, ProductController.dislike);
 
 module.exports = router;
